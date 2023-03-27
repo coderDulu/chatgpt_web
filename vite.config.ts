0199c55e4f7d440c5bca8ed2a784024dc4d71f59
codeDulu <dudu48021@gmail.com>
2023-03-27 09:40:53 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,27 @@ import viteCompression from 'vite-plugin-compression'
 
 
 // https://vitejs.dev/config/
+console.log(path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs'));
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin(), viteCompression()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, 'src'),
+      "@": path.join(__dirname, 'src'),
       'antd/dist/reset.css': path.join(__dirname, 'node_modules/antd/dist/reset.css'),  // <-- add this
       "antd": path.join(__dirname, 'node_modules/antd/dist/antd.js'),
       '@ant-design/icons': path.join(__dirname, 'node_modules\\@ant-design\\icons\\lib\\index.js'),
-      'react-syntax-highlighter/dist/cjs/styles/prism': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs/styles/prism/index.js'),
-      'react-syntax-highlighter': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs/index.js'),
+      // 'react-syntax-highlighter/styles/prism': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs/styles/prism'),
+      // 'react-syntax-highlighter': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs'),
     }
   },
   server: {
     proxy: {
       "/socket": {
-        target: "ws://127.0.0.1:3200",
+        target: "ws://167.88.186.119:3200",
         ws: true
       }
     }
   },
-
   build: {
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
